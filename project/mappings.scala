99f4ea0ff5d9458896ad8c8566db021cfb53b974
Nick Hibberd <nhibberd@gmail.com>
2014-11-05 14:40:42 +1100

    keep type parameters

--- project/mappings.scala
+++ project/mappings.scala
@@ -5,8 +5,13 @@ object Mappings {
   val dont =
     Seq("-dontoptimize", "-dontshrink", "-dontpreverify")
 
+  val keepAttributesAndParameters =
+    Seq("-keepattributes Exceptions,InnerClasses,Signature,Deprecated,SourceFile,LineNumberTable,*Annotation*,EnclosingMethod"
+      , "-keepparameternames")
+
   def preshim(update: UpdateReport) = {
     Seq(update.select(module = moduleFilter(organization = "com.amazonaws")).map(f => s"-injars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
+    keepAttributesAndParameters ++
     Seq("-ignorewarnings"
       , "-printmapping mappings.map"
       , "-keep class com.amazonaws.** { *; }"
@@ -23,6 +28,7 @@ object Mappings {
     val t = s.map(_.replace("-> com.amazonaws", "-> com.ambiata.com.amazonaws"))
     IO.writeLines(file.getParentFile / "proguard" / "aws.map", t)
     Seq(update.select(module = moduleFilter(organization = "com.amazonaws")).map(f => s"-injars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
+    keepAttributesAndParameters ++
     Seq("-ignorewarnings"
       , "-keep class com.amazonaws.** { *; }"
       , "-keepclassmembers class com.amazonaws.** { *; }"
