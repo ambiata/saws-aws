a962627776647125dccde0dbc750cde4b09d7b64
Nick Hibberd <nhibberd@gmail.com>
2014-11-14 11:33:55 +1100

    Adding dependencies to fatjar

--- project/mappings.scala
+++ project/mappings.scala
@@ -13,11 +13,20 @@ object Mappings {
 
   def preshim(update: UpdateReport) = {
     Seq(update.select(module = moduleFilter(organization = "com.amazonaws")).map(f => s"-injars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
+    Seq(update.select(module = moduleFilter(organization = "org.apache.httpcomponents")).map(f => s"-injars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
+    Seq(update.select(module = moduleFilter(organization = "commons-logging")).map(f => s"-injars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
+    Seq(update.select(module = moduleFilter(organization = "com.fasterxml.jackson.core")).map(f => s"-injars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
     keepAttributesAndParameters ++
     Seq("-ignorewarnings"
       , "-printmapping mappings.map"
       , "-keep class com.amazonaws.** { *; }"
       , "-keepclassmembers class com.amazonaws.** { *; }"
+      , "-keep class org.apache.commons.logging.** { *; }"
+      , "-keepclassmembers class org.apache.commons.logging.** { *; }"
+      , "-keep class org.apache.http.** { *; }"
+      , "-keepclassmembers class org.apache.http.** { *; }"
+      , "-keep class com.fasterxml.jackson.** { *; }"
+      , "-keepclassmembers class com.fasterxml.jackson.** { *; }"
       , "-libraryjars <java.home>/lib/rt.jar") ++
     Seq(update.select().map(f => s"-libraryjars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
     Seq("-outjars empty.jar") ++
@@ -27,13 +36,26 @@ object Mappings {
   def shim(name: String, version: String, update: UpdateReport, file: File) = {
     val r = IO.readLines(file.getParentFile / "proguard" / "mappings.map")
     val s = r.filter(!_.startsWith(" "))
-    val t = s.map(_.replace("-> com.amazonaws", "-> com.ambiata.com.amazonaws"))
+    val aws = s.map(_.replace("-> com.amazonaws", "-> com.ambiata.com.amazonaws"))
+    val log = aws.map(_.replace("-> org.apache.commons.logging", "-> com.ambiata.org.apache.commons.logging"))
+    val http = log.map(_.replace("-> org.apache.http", "-> com.ambiata.org.apache.http"))
+    val xml = http.map(_.replace("-> com.fasterxml.jackson", "-> com.ambiata.com.fasterxml.jackson"))
+    val t = xml
     IO.writeLines(file.getParentFile / "proguard" / "aws.map", t)
     Seq(update.select(module = moduleFilter(organization = "com.amazonaws")).map(f => s"-injars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
+    Seq(update.select(module = moduleFilter(organization = "org.apache.httpcomponents")).map(f => s"-injars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
+    Seq(update.select(module = moduleFilter(organization = "commons-logging")).map(f => s"-injars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
+    Seq(update.select(module = moduleFilter(organization = "com.fasterxml.jackson.core")).map(f => s"-injars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
     keepAttributesAndParameters ++
     Seq("-ignorewarnings"
       , "-keep class com.amazonaws.** { *; }"
       , "-keepclassmembers class com.amazonaws.** { *; }"
+      , "-keep class org.apache.commons.logging.** { *; }"
+      , "-keepclassmembers class org.apache.commons.logging.** { *; }"
+      , "-keep class org.apache.http.** { *; }"
+      , "-keepclassmembers class org.apache.http.** { *; }"
+      , "-keep class com.fasterxml.jackson.** { *; }"
+      , "-keepclassmembers class com.fasterxml.jackson.** { *; }"
       , "-applymapping aws.map"
       , "-libraryjars <java.home>/lib/rt.jar") ++
     Seq(update.select().map(f => s"-libraryjars $f(!META-INF/MANIFEST.MF)").mkString("\n")) ++
